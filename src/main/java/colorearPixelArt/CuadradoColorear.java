c8e601e64ac9eb67f0f36c4e853547470623753a
Cristian <chiat.contact@gmail.com>
2024-02-05 19:49:49 -0300

    comit inicial ya esta todo listo

---
+++ src/main/java/colorearPixelArt/CuadradoColorear.java
@@ -0,0 +1,6 @@
+package yosolito.pixelartfx;
+
+import javafx.scene.control.Button;
+
+public class CuadradoColorear extends Button {
+}
---
+++ src/main/java/colorearPixelArt/EscenaPixelArt.java
@@ -0,0 +1,14 @@
+package yosolito.pixelartfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EscenaPixelArt {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
